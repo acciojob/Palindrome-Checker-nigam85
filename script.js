0fb929cfb211cf22b4d0ea1b51d1cc4b9d76b3e6
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-15 12:24:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // complete the given function
 
 function palindrome(str){
-
+	 s = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  
+  // Compare the string with its reverse
+  return str === str.split("").reverse().join("");
 }
 module.exports = palindrome
